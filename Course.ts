c8eb8faa4754c3b6822baea3c3f34fd97eaf6c4c
Jordan Jones <jordanjones0902@gmail.com>
2023-10-06 09:15:57 -0400

    Learning about declaring  strings

---
+++ Course.ts
@@ -0,0 +1,9 @@
+// Declaring a string in JS 
+const MyVariable = 'Is Great';
+// Declaring a string in TS
+const MyVariable: string = 'Is Great';
+/* string is all lowercase 
+Can't reassign to a different type:
+ let mystring: string = 'Is Great';
+ mystring = 100;
+*/
