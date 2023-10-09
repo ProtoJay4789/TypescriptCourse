f169778ff9b65fbd2326069417fe52b99b5dc5cb
Jordan Jones <jordanjones0902@gmail.com>
2023-10-09 13:49:08 -0400

    Any  can used for any type, but is not recommended

--- Course.ts
+++ Course.ts
@@ -25,3 +25,19 @@ myBoolean = false;
 const MyBoolean: boolean = true;
 // Declaring a Boolean in JS
 const MyBoolean = true;
+//Type inference makes Typescript the goat
+let tvshow = 'Game of Thrones';
+tvshow = 'Breaking Bad';
+// Notice how TS is smart enough to know that the type is a string.
+// any can be used a shield to types, although it is not recommended.
+let myString: any = 'Is Great';
+const movies = ['Game of Thrones', 'Breaking Bad', 'The Lord of the Rings'];
+let foundMovie; // Any type
+
+
+
+for(let movie of movies){
+  if(movie === 'Game of Thrones'){
+    foundMovie = 'Game of Thrones';
+    // FoundMovie is now of type 'Game of Thrones'
+    // also can also be number, string, boolean, array, object, etc.
