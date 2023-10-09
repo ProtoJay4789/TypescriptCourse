079e95910b6f39c06d70acf04788aae63c3ed5fd
Jordan Jones <jordanjones0902@gmail.com>
2023-10-09 13:09:05 -0400

    Learning about Compiling to javascript

---
+++ Course.js
@@ -0,0 +1,27 @@
+/* string is all lowercase
+Can't reassign to a different type:
+ let mystring: string = 'Is Great';
+ mystring = 100;
+*/
+// Declaring a string in JS 
+var MyVariable = 'Is Great';
+// Declaring a string in TS
+var MyVariable = 'Is Great';
+/* Numbers work the same way in JS
+Can't reassign to a different type:
+let myNumber: number = 42;
+myNumber = "Cannot change myNumber"*/
+// Declaring a number in TS
+var MyNumber = 42;
+// Declaring a number in JS
+var MyNumber = 42;
+/* Can't reassign to a different type:
+let myBoolean: boolean = true;
+myBoolean = 56;
+Can reassign to a different value of the same type:
+myBoolean = false;
+*/
+// Declaring a Boolean in TS
+var MyBoolean = true;
+// Declaring a Boolean in JS
+var MyBoolean = true;
